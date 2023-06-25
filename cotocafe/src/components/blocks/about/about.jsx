6bd3433b2bd24b070a796c9c6cde81ef398319b7
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-06-25 14:49:22 +0300

    feat: добавить стили секции About

--- cotocafe/src/components/blocks/about/about.jsx
+++ cotocafe/src/components/blocks/about/about.jsx
@@ -1,24 +1,28 @@
 import React from "react";
-import './about.css';
+import "./about.css";
 import Title, { TitleSize } from "../../ui/title/title";
 
 function About() {
   return (
     <section className="about">
       <div className="about__wrapper">
-        <Title size={TitleSize.BIG}>Первое в России котокафе</Title>
-        <p className="about__text">
-          Крупнейшее котокафе России, в котором живёт 50 кошек и котов, и каждый
-          из них ищет новый дом. Животных можно гладить, фотографировать, играть
-          с ними.
-        </p>
-        <p className="about__work-time">Рабочее время с 8:00 до 23:00</p>
-        <p className="about__address">
-          Санкт-Петербург, набережная реки Карповки, дом 5, литера П
-        </p>
+        <div className="about__texts">
+          <Title size={TitleSize.BIG}>Первое в России котокафе</Title>
+          <p className="about__text">
+            Крупнейшее котокафе России, в котором живёт 50 кошек и котов, и каждый из них
+            ищет новый дом. Животных можно гладить, фотографировать, играть с ними.
+          </p>
+          <p className="about__text about__text_work-time">
+            Рабочее время с 8:00 до 23:00
+          </p>
+          <p className="about__text about__text_address">
+            Санкт-Петербург, набережная реки Карповки, дом 5, литера П
+          </p>
+        </div>
+        <div className="about__img"></div>
       </div>
     </section>
-  )
+  );
 }
 
 export default About;
